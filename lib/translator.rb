e41a6fcd097d482e15ed69ec76e171eeb462c038
John Easter <john@owltestprep.com>
2020-07-21 16:36:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
-def load_library(file_path)
-  emoticons = YAML.load_file('lib/emoticons.yml')
+def load_library(file_path = 'lib/emoticons.yml')
+  emoticons = YAML.load_file(file_path)
   return_hash = { "get_meaning" => {}, "get_emoticon" => {} }
   
   emoticons.each do |meaning, translations|
